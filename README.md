1d0061f64526b4152473b7ecdc84b10bd95ea60d
Honi <honihoneyhowni@gmail.com>
2025-02-18 02:32:16 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# barebones-arch
+
+I'll update this according to my necessities. As of right now only amdgpus are supported since I only use those
---
+++ post-install.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+print_recs() {
+    echo "1. Don't forget to install yay! (https://github.com/Jguer/yay.git)"
+    echo "2. Configure ly on /etc/ly/config.ini (clock format: %F %a - %r)"
+    echo "3. Don't forget to enable ly, cups and bluetooth!"
+    echo "4a. Create ssh keys and add them to your github account"
+    echo "4b. Clone your dotfiles with this command (Also make the alias <3): "
+    printf "\t%s\n" 'git clone --bare "$dotfiles_repo" "$HOME"/.dotfiles &>/dev/null'
+    printf "\talias dotfiles='%s'\n" '/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+}
+
+docli() {
+    echo "Installing aur cli"
+
+    mapfile -t cli <./packages/aur-cli.txt
+
+    yay -S --noconfirm "${cli[@]}"
+}
+
+dogui() {
+    echo "Installing gui"
+
+    mapfile -t gui <./packages/gui.txt
+    mapfile -t aur_gui <./packages/aur-gui.txt
+
+    sudo pacman -S --noconfirm "${gui[@]}"
+    yay -S --noconfirm "${aur_gui[@]}"
+    sleep 5
+    sudo localectl set-x11-keymap latam pc105 deadtilde
+}
+
+doconf() {
+    echo "Making some directories"
+    (
+        cd "$HOME" || exit
+        mkdir codino repos Documents/IRL
+    )
+}
+
+printf "%s\n" "Welcome!"
+PS3="Choose an option: "
+menu_opts=("Recommendations" "Finish cli install with aur" "Install GUI" "Configure some stuff")
+
+select opt in "${menu_opts[@]}"; do
+    case $opt in
+    "Recommendations")
+        print_recs
+        break
+        ;;
+    "Finish cli install with aur")
+        docli
+        break
+        ;;
+    "Install GUI")
+        dogui
+        break
+        ;;
+    "Configure some stuff")
+        doconf
+        break
+        ;;
+    *)
+        echo "Invalid option"
+        ;;
+    esac
+done
