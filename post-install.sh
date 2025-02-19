7389a6ee6c4fa999d3ae87e4d0af9d3fa2dafa11
Hownioni <honihoneyhowni@gmail.com>
2025-02-19 01:15:06 -0600

    Better recs

--- post-install.sh
+++ post-install.sh
@@ -4,10 +4,11 @@ print_recs() {
     echo "1. Don't forget to install yay! (https://github.com/Jguer/yay.git)"
     echo "2. Configure ly on /etc/ly/config.ini (clock format: %F %a - %r)"
     echo "3. Don't forget to enable ly, cups and bluetooth!"
-    echo "4a. Create ssh keys and add them to your github account"
+    echo "4a. Create ssh keys and add them to your github account (don't forget to activate ssh-agent and config ~/.ssh)"
     echo "4b. Clone your dotfiles with this command (Also make the alias <3): "
     printf "\t%s\n" 'git clone --bare "$dotfiles_repo" "$HOME"/.dotfiles &>/dev/null'
     printf "\talias dotfiles='%s'\n" '/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+    echo "Configure firefox (PENDING: Add which extensions I use)"
 }
 
 docli() {
